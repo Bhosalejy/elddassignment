a26d56b7ab0ddca19d467cd8f93564be9240c218
E1_Jyoti_83865 <jsbhosale.com>
2024-06-16 22:32:15 +0530

    elddassignment

--- day_8/ASS2/pchar.c
+++ day_8/ASS2/pchar.c
@@ -5,7 +5,6 @@
 #include <linux/cdev.h>
 #include <linux/kfifo.h>
 #include "pchar_ioctl.h"
-#include <linux/uaccess.h>
 
 static int pchar_open(struct inode *, struct file *);
 static int pchar_close(struct inode *, struct file *);
@@ -49,7 +48,7 @@ static __init int pchar_init(void) {
     minor = MINOR(devno);
     printk(KERN_INFO "%s: alloc_chrdev_region() allocated device number %d/%d.\n", THIS_MODULE->name, major, minor);
 
-    pclass = class_create(THIS_MODULE,"pchar_class");
+    pclass = class_create("pchar_class");
     if(IS_ERR(pclass)) {
         printk(KERN_ERR "%s: class_create() failed.\n", THIS_MODULE->name);
         ret = -1;
@@ -138,7 +137,10 @@ static ssize_t pchar_write(struct file *pfile, const char *ubuf, size_t size, lo
 
 static long pchar_ioctl(struct file *pfile, unsigned int cmd, unsigned long param) {
     info_t info;
-	int new_size;
+    void *temp_buf;
+    size_t old_size, new_size;
+    int ret = 0;
+    
     switch (cmd)
     {
     case FIFO_CLEAR:
@@ -154,24 +156,61 @@ static long pchar_ioctl(struct file *pfile, unsigned int cmd, unsigned long para
         break;
     case FIFO_RESIZE:
         printk(KERN_INFO "%s: pchar_ioctl() resize fifo.\n", THIS_MODULE->name); 
-        // allocate temp buffer of fifo length - kmalloc(), kifo_len()
-        void *temp_buffer = kmalloc(kfifo_len(&buf), GFP_KERNEL);
-        if (!temp_buffer) {
-        return -ENOMEM;
+        // Retrieve the new size from userspace
+      //  if (copy_from_user(&new_size, (size_t *)&param, sizeof(size_t))) {
+      //      ret = -EFAULT;
+       //     break;
+      //  }
+        
+        // Check if new size is valid
+        new_size = param;
+        printk(KERN_INFO " %ld the new size",new_size);
+        if (new_size <= 0) {
+            ret = -EINVAL;
+            break;
         }
-		// copy fifo data into that buffer - kfifo_out()
-        kfifo_out(&buf, temp_buffer, kfifo_len(&buf));
 
-		// release kfifo memory - kfifo_free()
+        // Allocate temporary buffer to hold current data
+        temp_buf = kmalloc(kfifo_len(&buf), GFP_KERNEL);
+        if (!temp_buf) {
+            ret = -ENOMEM;
+            break;
+        }
+
+        // Dequeue data from current kfifo to temp_buf
+        old_size = kfifo_len(&buf);
+        ret = kfifo_out(&buf, temp_buf, old_size);
+        if (ret != old_size) {
+            kfree(temp_buf);
+            ret = -EFAULT;
+            break;
+        }
+
+        // Free the old kfifo buffer
         kfifo_free(&buf);
-		// allocate kfifo with new size - kfifo_alloc() with param
-        kfifo_alloc(&buf, new_size, GFP_KERNEL);
 
-		// copy temp buffer data back to fifo - kfifo_in()
-		kfifo_in(&buf, temp_buffer, kfifo_len(&buf));
-		// release temp buffer - kfree()
-        kfree(temp_buffer);
-		break;
+        // Allocate new kfifo buffer with the new size
+        ret = kfifo_alloc(&buf, new_size, GFP_KERNEL);
+        if (ret) {
+            kfree(temp_buf);
+            ret = -ENOMEM;
+            break;
+        }
+
+        // Enqueue data from temp_buf to the new kfifo
+        ret = kfifo_in(&buf, temp_buf, old_size);
+        if (ret != old_size) {
+            kfifo_free(&buf);
+            kfree(temp_buf);
+            ret = -EFAULT;
+            break;
+        }
+
+        // Free the temporary buffer
+        kfree(temp_buf);
+        
+        printk(KERN_INFO "%s: pchar_ioctl() resized fifo to %zu bytes.\n", THIS_MODULE->name, new_size);
+        break;
     default:
         printk(KERN_ERR "%s: pchar_ioctl() unsupported command.\n", THIS_MODULE->name); 
         return -EINVAL;
